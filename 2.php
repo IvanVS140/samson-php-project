69b00ff6085467bc8f6d6d34cf2f9ccf9b46f8dd
Ivan <ivanvs140@gmail.com>
2021-09-17 18:14:07 +0300

    mySortForKey func added. Cosmetic fixes

--- 2.php
+++ 2.php
@@ -2,14 +2,14 @@
 
 echo "<pre>";
 
-$haystack = array(  "01qweabc234567defgqwe8910qwexyz",
-                    "01abc234567defgqwe8910qwexyz",
-                    "01abc234567defg8910qwexyz",
-                    "01abc234567defg8910xyz");
-$needle = "qwe";
+$haystack = array(  '01qweabc234567defgqwe8910qwexyz',
+                    '01abc234567defgqwe8910qwexyz',
+                    '01abc234567defg8910qwexyz',
+                    '01abc234567defg8910xyz');
+$needle = 'qwe';
 
 /**
- * Convert string
+ * Converts second occurrence substring into reverse
  *
  * @param mixed $a Source string
  * @param mixed $b Substring needle
@@ -29,6 +29,67 @@ function convertString($a, $b)
 echo "Substring: " . $needle  . str_repeat(PHP_EOL, 2);
 foreach ($haystack as $str) {
     echo $str . PHP_EOL . convertString($str, $needle) . str_repeat(PHP_EOL, 2);
+}
+
+/**
+ * Checks key existence and throws exception if it doesn't
+ *
+ * @param mixed $key Key to check
+ * @param array $arr Array to be checked
+ *
+ * @return void
+ */
+function keyExistence($key, $arr)
+{
+    if (!array_key_exists($key, $arr)) {
+        throw new OutOfRangeException("key [$key] is missing");
+    }
+}
+
+/**
+ * Sorting an array in ascending order of values for user key
+ *
+ * @param array $a Array to sort
+ * @param mixed $b User key
+ *
+ * @return array An array, sorted in ascending order of values for the user key
+ */
+function mySortForKey($a, $b)
+{
+    $error_count = 0;
+    foreach ($a as $key => $value) {
+        try {
+            keyExistence($b, $value);
+        } catch (OutOfRangeException $e) {
+            echo "Warning! The " . $e->getMessage() . " at index [$key]" .
+            PHP_EOL;
+            $error_count++;
+        }
+    }
+    if ($error_count == 0) {
+        $sort_arr = array();
+        foreach ($a as $key => $value) {
+            $sort_arr[$key] = $value[$b];
+        }
+        array_multisort($sort_arr, $a);
+        return $a;
+    } else {
+        echo "Total errors: $error_count";
+    }
+}
+
+$test_array = array(
+    [['a' => 0, 'b' => 3], ['b' => 1, 'a' => 5.4], ['a' => 5.3, 'b' => 2]],
+    [['a' => 0, 'b' => 3], ['c' => 1, 'a' => 5.4], ['a' => 5.3, 'b' => 2]],
+    [['a' => 0, 'c' => 3], ['b' => 1, 'a' => 5.4], ['a' => 5.3, 'c' => 2]],
+    [['a' => 0, 'c' => 3], ['c' => 1, 'a' => 5.4], ['a' => 5.3, 'c' => 2]]
+);
+
+$user_key = 'b';
+
+foreach ($test_array as $arr) {
+    print_r(mySortForKey($arr, $user_key));
+    echo str_repeat(PHP_EOL, 2);
 }
 
 // phpcs:ignore PSR2.Files.ClosingTag.NotAllowed
