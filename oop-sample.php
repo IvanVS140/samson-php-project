3f5866f4c77fd44e73fb62e89ea3012c6ac9a8ba
Ivan <ivanvs140@gmail.com>
2022-03-13 16:21:15 +0300

    develop branch init

---
+++ oop-sample.php
@@ -0,0 +1,95 @@
+<?php // phpcs:ignore PSR1.Files.SideEffects.FoundWithSymbols
+
+namespace OOP_Sample;
+
+echo "<pre>";
+
+/**
+ * WeatherEntry
+ *
+ * @category OOP_Sample
+ * @package  OOP_Sample
+ * @author   Ivan Shevyrev <ivanvs140@mail.ru>
+ * @license  http://www.opensource.org Test3
+ * @link     https://github.com
+ */
+class WeatherEntry
+{
+    private $date; // phpcs:ignore
+    private $comment = ""; // phpcs:ignore
+    private $temperature = 0; // phpcs:ignore
+    private $isRainy = false; // phpcs:ignore
+
+    /**
+     * __construct
+     *
+     * @param mixed $date        comment here
+     * @param mixed $comment     comment here
+     * @param mixed $temperature comment here
+     *
+     * @return void
+     */
+    public function __construct($date, string $comment, int $temperature)
+    {
+        $this->date = $date;
+        $this->comment = $comment;
+        $this->temperature = $temperature;
+    }
+
+    /**
+     * Is Cold?
+     *
+     * @return void
+     */
+    public function isCold()
+    {
+        return $this->temperature < 0;
+    }
+
+    /**
+     * Set rain status
+     *
+     * @param mixed $rain_status comment here
+     *
+     * @return void
+     */
+    public function setRainStatus($rain_status)
+    {
+        $this->isRainy = $rain_status;
+    }
+
+    /**
+     * Get day description
+     *
+     * @return void
+     */
+    public function getDayDescription()
+    {
+        $dt = strtotime($this->date);
+        $delta = time() - $dt;
+        $days = ceil($delta / 86400);
+
+        $qwe = $this->comment;
+
+        $res = "Это было $days дней назад. $qwe. В тот день было ";
+
+        if ($this->isCold()) {
+            $res .= "холодно. ";
+        } else {
+            $res .= "довольно тепло. ";
+        }
+
+        if ($this->isRainy) {
+            $res .= "Семенил дождь.";
+        } else {
+            $res .= "На небе не было ни облачка.";
+        }
+
+        return $res;
+    }
+}
+
+$firstSeptember = new WeatherEntry("2018-09-01", "День знаний", 14);
+$firstSeptember->setRainStatus(false);
+
+print($firstSeptember->getDayDescription());
